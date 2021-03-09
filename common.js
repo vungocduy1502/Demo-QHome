2a8fed50ed4a3d9d84f0cf140a0f66c71b7cc325
Anh Duy <anhduy@Anh-Duy.local>
2021-03-09 23:17:11 +0700

    first commit

---
+++ common.js
@@ -0,0 +1,46 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      moreText.style.display = "none";
+      btnText.innerHTML = "Đọc tiếp"; 
+     
+    } else {
+      dots.style.display = "none";
+      moreText.style.display = "inline";
+      btnText.innerHTML = "Thu gọn"; 
+      
+    }
+  }
+
+  function myFunction1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Đọc tiếp"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Thu gọn"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  $(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideToggle("slow");
+    });
+  });
+  $(document).ready(function(){
+    $("#flip1").click(function(){
+      $("#panel1").slideToggle("slow");
+    });
+  });
+
+ 
